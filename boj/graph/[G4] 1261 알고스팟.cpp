e9b686d26ff8c42850a7f56d730a3034aab482b0
xcelxlorx <96944649+xcelxlorx@users.noreply.github.com>
2024-01-16 09:36:52 +0900

    Create [G4] 1261 알고스팟.cpp

---
+++ boj/graph/[G4] 1261 알고스팟.cpp
@@ -0,0 +1,55 @@
+//bfs + 다익스트라
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#define INF 987654321
+
+using namespace std;
+
+vector<string> v;
+int m, n;
+int dx[4] = { 0, 0, 1, -1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+int dijkstra() {
+	queue<pair<int, int>> q; //시작, 끝, 부순 벽의 개수
+	vector<vector<int>> dist(n, vector<int>(m, INF));
+  
+  q.push({ 0, 0 });
+	dist[0][0] = 0;
+
+	while (!q.empty()) {
+		auto p = q.front();
+		int x = p.first, y = p.second;
+		q.pop();
+
+		if (answer < dist[x][y]) continue;
+
+		for (int k = 0; k < 4; k++) {
+			int nx = x + dx[k], ny = y + dy[k];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+			int state = v[nx][ny] == '1' ? 1 : 0;
+			if (dist[nx][ny] > state + dist[x][y]) {
+				dist[nx][ny] = state + dist[x][y];
+				q.push({ nx, ny });
+			}		
+		}
+	}
+	return dist[n - 1][m - 1];
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> m >> n;
+
+	v.resize(n);
+
+	for (int i = 0; i < n; i++) cin >> v[i];
+
+	cout << dijkstra();
+}
